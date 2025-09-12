d05a5452ca4dd487f7f603e6cd47ae42ddcd84d0
PersimMonMon <buitho@oregonstate.edu>
2025-09-11 17:34:00 -0700

    create products file

--- view/src/App.jsx
+++ view/src/App.jsx
@@ -8,26 +8,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        Vote2Restock
+      </header>
+      
+      <div className="container">
+        <div className="main-section">
+          <p>hello</p>
+        </div>
+        <div className="side-section">
+          <p>hello</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
